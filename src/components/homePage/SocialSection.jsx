8cc671c66ab029dedd6ab169cb3f117c3d857aaa
MxKzCharlie <mxkzcharlie01@gmail.com>
2024-12-16 12:10:14 -0800

    add link to LinkedIn

--- src/components/homePage/SocialSection.jsx
+++ src/components/homePage/SocialSection.jsx
@@ -1,6 +1,6 @@
 import '../../assets/css/homePage/socialSection.css'
 import { FaInstagram, FaGithub, FaLinkedin, FaWhatsapp } from "react-icons/fa"
-import { linkIg, linkGit, linkWhats } from '../../utils/links'
+import { linkIg, linkGit, linkWhats, linkIn } from '../../utils/links'
 import { Link } from 'react-router'
 
 function SocialSection() {
@@ -8,7 +8,7 @@ function SocialSection() {
         <section className="socialSection">
             <Link to={linkIg} target="_blank"><FaInstagram className="social-icon" /></Link>
             <Link to={linkGit} target="_blank"><FaGithub className="social-icon" /></Link>
-            <Link ><FaLinkedin className="social-icon" /></Link>
+            <Link to={linkIn} target="_blank"><FaLinkedin className="social-icon" /></Link>
             <Link to={linkWhats} target="_blank"><FaWhatsapp className="social-icon" /></Link>
         </section>
     );
--- src/utils/links.js
+++ src/utils/links.js
@@ -1,4 +1,4 @@
 export const linkIg = 'https://www.instagram.com/carcruz18/';
 export const linkGit = 'https://github.com/MxKzCharlie';
 export const linkWhats = 'https://wa.me/526643367495?text=Hola%20Carlos%2C%20quiero%20contactarte%20sobre%20un%20proyecto!';
-export const linkIn = '';
+export const linkIn = 'https://www.linkedin.com/in/carlos-cruz-b36212342';
